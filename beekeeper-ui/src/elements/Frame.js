a70d5ba8966c1680be4d7722b673e5d1d5928514
mario <mariusz.sygnowski@gmail.com>
2021-08-21 21:24:00 +0200

    some react ui stuff

---
+++ beekeeper-ui/src/elements/Frame.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import List from '@meterial-ui/core'
+
+
+export default function Frame (props) {
+
+
+    return (
+        <List component="nav">
+            
+        </List>
+    )
+}
